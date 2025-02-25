1c982c1f9a5824b0104d0c9405c0f063795f7ed0
Vojta <149485613+VojtaMatej@users.noreply.github.com>
2025-02-25 14:51:32 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,10 +12,7 @@ app.use(express.json());
 app.use(cors());
 
 // Připojení k MongoDB
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI);
 .then(() => console.log('✅ Připojeno k MongoDB'))
 .catch(err => console.error('❌ Chyba připojení k MongoDB:', err));
 
